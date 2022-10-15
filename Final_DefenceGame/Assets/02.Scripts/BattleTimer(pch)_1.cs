9bee7fe4f59e81d9c7e280426320d7f6f4eaa42b
gachonkyronko <115342215+gachonkyronko@users.noreply.github.com>
2022-10-15 22:46:33 +0900

    adit_merge

--- Final_DefenceGame/Assets/02.Scripts/BattleTimer(pch)_1.cs
+++ Final_DefenceGame/Assets/02.Scripts/BattleTimer(pch)_1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class BattleTimer : MonoBehaviour
+public class BattleTimer_pch : MonoBehaviour
 {
     public float setTime = 20.0f;
     public Text countdowntext;
