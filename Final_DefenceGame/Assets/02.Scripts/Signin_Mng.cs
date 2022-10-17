681affbe3952b87c6639962fcf6ed906d697ee3a
gachonkyronko <115342215+gachonkyronko@users.noreply.github.com>
2022-10-17 19:46:15 +0900

    닉네임출력완성

--- Final_DefenceGame/Assets/02.Scripts/Signin_Mng.cs
+++ Final_DefenceGame/Assets/02.Scripts/Signin_Mng.cs
@@ -8,16 +8,29 @@ using PlayFab.ClientModels;
 
 public class Signin_Mng : MonoBehaviour
 {
+     
     public InputField EmailInput;
     public InputField PasswordInput;
     public bool loginok = false;
+    public static string myID="";
+    public static string myEmail = "";
+    public static string myPassword = "";
 
     public void SigninBtn() //로그인버튼
     {
+        myEmail = EmailInput.text;
+        myPassword = PasswordInput.text;
         var request = new LoginWithEmailAddressRequest { Email = EmailInput.text, Password = PasswordInput.text };
-        PlayFabClientAPI.LoginWithEmailAddress(request, (result) => loginok = true , (error) => print("로그인 실패"));
+        PlayFabClientAPI.LoginWithEmailAddress(request, (result) => { loginok = true; myID = result.PlayFabId;  }, (error) => print("로그인 실패"));
+        
+       
     }
-    private void Update() //로그인버튼활성화시 바로 씬 넘어가지도록
+    
+
+     
+
+
+        private void Update() //로그인버튼활성화시 바로 씬 넘어가지도록
     {
         if(loginok==true)
         {
--- Final_DefenceGame/Assets/02.Scripts/Signup_Mng.cs
+++ Final_DefenceGame/Assets/02.Scripts/Signup_Mng.cs
@@ -11,16 +11,41 @@ public class Signup_Mng : MonoBehaviour
     public InputField PasswordInput;
     public InputField UsernameInput;
     public bool signupok = false;
-
+    public bool signupfinish = false;
+    public bool nicknamefinish = false;
+    public string Username="";
     public void RegisterBtn() //회원가입버튼
     {
-        var request = new RegisterPlayFabUserRequest { Email = EmailInput.text, Password = PasswordInput.text, Username = UsernameInput.text };
-        PlayFabClientAPI.RegisterPlayFabUser(request, (result) => signupok = true, (error) => print("회원가입 실패"));
+         
+        var request = new RegisterPlayFabUserRequest { Email = EmailInput.text, Password = PasswordInput.text, Username = UsernameInput.text};
+        PlayFabClientAPI.RegisterPlayFabUser(request, (result) => { signupfinish = true; nickname(); }, (error) => print("회원가입 실패")) ;
+        Username = UsernameInput.text;
+        
+         
     }
     void Update() //회원가입성공시 화면이동
     {
         if (signupok == true)
             SceneManager.LoadScene("Signin_Scene");
+         
+    }
+    void nickname()
+    {
+        if (signupfinish == true)
+        {
+            var request = new LoginWithEmailAddressRequest { Email = EmailInput.text, Password = PasswordInput.text };
+            PlayFabClientAPI.LoginWithEmailAddress(request, (result) => { nicknamefinish = true; print("로그인성공");nicknameok();  }, (error) => print("로그인 실패"));
+             
+        }
+    }
+    void nicknameok()
+    {
+        if(nicknamefinish == true)
+        {
+             
+            var request1 = new UpdateUserDataRequest() { Data = new Dictionary<string, string>() { { "닉네임", Username } } };
+            PlayFabClientAPI.UpdateUserData(request1, (result) => { print("데이터 저장 성공");signupok = true; }, (error) => print("데이터 저장 실패"));
+        }
     }
     public void BackBtn() //뒤로가기
     {
